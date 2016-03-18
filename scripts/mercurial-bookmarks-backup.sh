ec13e19bf7a4276243b9017b715cfe72c262772b
Luca Greco <luca.greco@alcacoop.it>
2016-03-18 18:56:50 +0100

    tweak mercurial-bookmarks-backup.sh

--- scripts/mercurial-bookmarks-backup.sh
+++ scripts/mercurial-bookmarks-backup.sh
@@ -3,11 +3,6 @@ DEST_DIR=$1
 BOOKMARKS=`hg bookmarks | awk '{ print $1 }' | grep -v fx-team`
 IS_DIRTY=`hg status -madu | wc -l`
 
-if test $IS_DIRTY -gt 0 ; then
-  echo "Mercurial Repo is DIRTY."
-  exit 1
-fi
-
 current_bookmark() {
   hg log --template '{if(currentbookmark, "{currentbookmark}", if(fxheads, "{fxheads}", "{tags}"))}' -r .
 }
@@ -15,6 +10,8 @@ current_bookmark() {
 OLD_BOOKMARK=`current_bookmark`
 
 backup_bookmark() {
+  echo "backup BOOKMARK: $1"
+
   local bookmark_dir=`echo $1 | cut -d / -f 1`
   mkdir -p $DEST_DIR/$1
 
@@ -23,10 +20,21 @@ backup_bookmark() {
   hg export -g -r 'pending' -o "$DEST_DIR/$1/%n-%m.patch"
 }
 
-for bookmark in $BOOKMARKS; do
-  echo "backup BOOKMARK: $bookmark"
-  backup_bookmark $bookmark
-done
+if test "$2" = "all" ; then
+  if test $IS_DIRTY -gt 0 ; then
+    echo "ERROR: your repo is currently dirty"
+    exit 1
+  fi
 
-echo RESTORE OLD_BOOKMARK: $OLD_BOOKMARK
-hg update -r $OLD_BOOKMARK
+  echo "Backup of all the bookmarks? (^C to exit)"
+  read
+
+  for bookmark in $BOOKMARKS; do
+    backup_bookmark $bookmark
+  done
+
+  echo "restoring BOOKMARK: $OLD_BOOKMARK"
+  hg update -r $OLD_BOOKMARK
+else
+  backup_bookmark $OLD_BOOKMARK
+fi
